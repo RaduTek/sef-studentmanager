72fc8d2faff6bc5526b817b21ba3a73ba6b7bc26
RaduTek <me@radutek.uk>
2025-03-24 15:30:21 +0200

    SM-1 Added the login screen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.radutek.sefstudentmanager {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens loose.oose.fis.lab.student.manager to javafx.fxml;
     exports loose.oose.fis.lab.student.manager;
+
+    opens loose.oose.fis.lab.student.manager.controllers to javafx.fxml;
+    exports loose.oose.fis.lab.student.manager.controllers to javafx.fxml;
 }
